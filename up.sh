993a69c0900ce59e9d6997c75c0b202e1f5febb7
BLACK_HYDRA <112259891+BlackHydra40@users.noreply.github.com>
2022-09-20 18:27:30 -0300

    Add files via upload

--- up.sh
+++ up.sh
@@ -1,12 +1,14 @@
-
+clear
 cd
 rm -rf $HOME/Black_CC
 figlet "ATUALIZANDO" | lolcat
+echo "saiba mais sobre novas atalizações"
+echo "https://github.com/BlackHydra40/Black_CC"
 echo
 
 git clone https://github.com/BlackHydra40/Black_CC
 cd Black_CC
+clear
+python3 black.py
 
-echo
-figlet "UPDATE COMPLETO" | lolcat
 sleep 3
